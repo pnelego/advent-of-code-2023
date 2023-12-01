daf9c8db938f3f281fbf90763d87ce6291664586
pnelego <huckabywyatt@gmail.com>
2023-11-30 21:47:14 -0700

    last min template adjustment to make sure I get an accurate timer

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -9,7 +9,13 @@
 #include <string>
 
 
-void process(const std::vector<std::string>& lines)
+
+void part_one(const std::vector<std::string> &lines)
+{
+}
+
+
+void part_two(const std::vector<std::string> &lines)
 {
 }
 
@@ -19,16 +25,30 @@ int main()
 {
     //read input file
     std::vector<std::string> lines = FileLoader::load_file("input.txt");
+    //in order to get accurate time measurements we need to have two seperate vectors
+    //one for each part
+    std::vector<std::string> lines2 = FileLoader::load_file("input.txt");
+
 
     Timer timer;
 
     timer.start();
 
-    //process input
-    process(lines);
+    //we're gonna make a copy with both parts so one doesn't affect the other
+    part_one(lines);
 
     //stop timer
     const uint64_t timeTaken = timer.stop();
-    std::cout << "Time taken: " << timeTaken << "ms" << std::endl;
+    std::cout << "Part one time: " << timeTaken << "ms" << std::endl;
+
+    timer.start();
+
+    part_two(lines2);
+
+    //stop timer
+    const uint64_t timeTaken2 = timer.stop();
+    std::cout << "Part two time: " << timeTaken2 << "ms" << std::endl;
+
+
     return 0;
 }
